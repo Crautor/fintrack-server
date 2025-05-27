e9124f2385c34b94b4beb563eb96d74ffbd20dc8
JESUISzin <guto3210123@gmail.com>
2025-05-26 21:31:42 -0300

    prettier

--- financialGoal/src/interfaces/validators/financialGoalValidator.js
+++ financialGoal/src/interfaces/validators/financialGoalValidator.js
@@ -4,6 +4,9 @@ export default yup.object().shape({
   userId: yup.string().required('User ID is required'),
   value: yup.number().positive('Must be positive (value)').required('Required (value)'),
   limitDate: yup.date().required('Required (limitDate)'),
-  status: yup.string().oneOf(['Aberto', 'Em_Andamento', 'Expirado', 'Concluido'], 'Invalid (status)').required('Required (status)'),
+  status: yup
+    .string()
+    .oneOf(['Aberto', 'Em_Andamento', 'Expirado', 'Concluido'], 'Invalid (status)')
+    .required('Required (status)'),
   title: yup.string().max(100, 'Max length is 100 characters (title)').required('Required (title)'),
 });

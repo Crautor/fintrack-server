b37a61ffed0c660cb61bb3e251119402fe9efbe1
Vitor Dallanol <vitor.dallanol@gmail.com>
2025-06-09 23:07:13 -0300

    fix: categoria

--- category/src/domain/entities/Category.js
+++ category/src/domain/entities/Category.js
@@ -1,7 +1,8 @@
 export default class Category {
-  constructor({ categoryId, name, icon }) {
+  constructor({ categoryId, name, icon, userId }) {
     this.categoryId = categoryId;
     this.name = name;
     this.icon = icon;
+    this.userId = userId;
   }
 }

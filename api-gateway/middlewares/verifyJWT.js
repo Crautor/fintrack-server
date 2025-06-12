9063d73081870506ad53963d3972e46c4c196482
Vitor Dallanol <vitor.dallanol@gmail.com>
2025-06-12 00:46:52 -0300

    fix: linter

--- api-gateway/middlewares/verifyJWT.js
+++ api-gateway/middlewares/verifyJWT.js
@@ -1,6 +1,5 @@
 import jwt from 'jsonwebtoken';
 
-
 export default (req, res, next) => {
   const token = req.headers['authorization']?.split(' ')[1];
   if (!token) return res.unauthorized().json({ error: 'No token' });
